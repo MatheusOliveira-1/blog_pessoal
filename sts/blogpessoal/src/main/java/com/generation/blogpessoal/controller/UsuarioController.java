a5e9e9d5871fa67093c8625e24b0758005b6d3a0
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-03-06 16:47:03 -0300

    finalizado o CRUD inserindo o metodo delete

--- sts/blogpessoal/src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ sts/blogpessoal/src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -5,15 +5,11 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
-import com.generation.blogpessoal.model.Usuario;
-import com.generation.blogpessoal.model.UsuarioLogin;
-import com.generation.blogpessoal.repository.UsuarioRepository;
-import com.generation.blogpessoal.service.UsuarioService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +17,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.model.UsuarioLogin;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+import com.generation.blogpessoal.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -96,6 +97,19 @@ public class UsuarioController {
 		return usuarioService.atualizarUsuario(usuario)
 			.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
 			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<?> deleteUsuario(@PathVariable Long id){
+		if (usuarioRepository.existsById(id)) {
+			return usuarioRepository.findById(id).
+					map(res -> {
+						usuarioRepository.deleteById(id);
+						return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+					})
+					.orElse(ResponseEntity.notFound().build());
+		}
+		return ResponseEntity.notFound().build();
 	}
 
 }
