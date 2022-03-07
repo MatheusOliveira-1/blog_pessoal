5a37f819e242faecb142444178641c9d0380645a
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-03-07 12:00:22 -0300

    update no metodo atualizarUsuario da classe service Usuario

--- sts/blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ sts/blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -3,14 +3,16 @@ package com.generation.blogpessoal.service;
 import java.nio.charset.Charset;
 import java.util.Optional;
 
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.generation.blogpessoal.model.Usuario;
 import com.generation.blogpessoal.model.UsuarioLogin;
 import com.generation.blogpessoal.repository.UsuarioRepository;
-
-import org.apache.commons.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
 /**
  *  A Classe UsuarioService implementa as regras de negócio do Recurso Usuario.
@@ -85,10 +87,24 @@ public class UsuarioService {
 	 */
 	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
 		
-		if(usuarioRepository.findById(usuario.getId()).isPresent()) {
+if(usuarioRepository.findById(usuario.getId()).isPresent()) {
 			
 			/**
-		 	* Se o Usuário existir no Banco de Dados, a senha será criptografada
+			 * Cria um Objeto Optional com o resultado do método findById
+			 */
+			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
+			
+			/**
+			 * Se o Usuário existir no Banco de dados e o Id do Usuário encontrado no Banco for 
+			 * diferente do usuário do Id do Usuário enviado na requisição, a Atualização dos 
+			 * dados do Usuário não pode ser realizada.
+			 */
+			if ( (buscaUsuario.isPresent()) && ( buscaUsuario.get().getId() != usuario.getId()))
+				throw new ResponseStatusException(
+						HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+
+			/**
+		 	* Se o Usuário existir no Banco de Dados e o Id for o mesmo, a senha será criptografada
 		 	* através do Método criptografarSenha.
 		 	*/
 			usuario.setSenha(criptografarSenha(usuario.getSenha()));
